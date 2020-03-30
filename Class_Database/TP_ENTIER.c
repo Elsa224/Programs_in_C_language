4f8206dae54219b0f8d34ee8ae803a75918ea26b
Elsa224 <elsazougouri224@gmail.com>
2020-03-30 12:55:01 +0000

    editing some comments and indent spaces

--- Class_Database/TP_ENTIER.c
+++ Class_Database/TP_ENTIER.c
@@ -1,30 +1,29 @@
 /*TP final : Gestion d'une classe étudiants en utilisant une base de données (fichiers)*/
 
+//Inclure le fichier des librairies et de toutes les procédures et fonctions utilisées
 #include "variables_functions.h"
 
-///Etape (i) : Création de la BD de 10 étudiants dans un fichier
 int main(int argc, char const *argv[])
 {
-   char result = menuDeTravail();
-   //Ensuite mettre les procédures des différentes tâches
-   switch (result)
-   {
-      case '1':
-        creationFICH_VRAC(fichierBD_initial, prepa2);
-        creationFICH_BASE(fichierBD_initial, prepa2, nbStudentInFile(fichVRAC));
-      break;
+    char result = menuDeTravail(); //Pour avoir le choix de l'utilisateur
+    switch (result)
+    {
+        case '1': //Créer FICHIER_VRAC
+            creationFICH_VRAC(fichierBD, prepa2);
+            creationFICH_BASE(fichierBD, prepa2, nbStudentInFile(fichVRAC));
+        break;
 
-      case '2':
-        ajoutEtudiant(fichierBD_initial, prepa2, nbStudentInFile(fichBASE));
-      break;
+        case '2': //Ajout d'un étudiant
+            ajoutEtudiant(fichierBD, prepa2, nbStudentInFile(fichBASE));
+        break;
 
-      case '3':
-        correctionEmail(fichierBD_initial, prepa2, nbStudentInFile(fichBASE));
-      break;
-         
-      case '4':
-        creationFICH_FINAL(fichierBD_initial, prepa2,nbStudentInFile(fichBASE));
-      break;
-   }
-   return 0;
+        case '3': //Corriger l'adresse email
+            correctionEmail(fichierBD, prepa2, nbStudentInFile(fichBASE));
+        break;
+            
+        case '4': //Créer FICHIER_FINAL
+            creationFICH_FINAL(fichierBD, prepa2,nbStudentInFile(fichBASE));
+        break;
+    }
+    return 0;
 }
